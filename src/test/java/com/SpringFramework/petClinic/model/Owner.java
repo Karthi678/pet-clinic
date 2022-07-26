8710b574c7c432e4b33189f3ace45d0258868e20
Karthi678 <1801023c@cit.edu.in>
2022-07-26 17:43:39 +0530

    adding POJO's.

---
+++ src/test/java/com/SpringFramework/petClinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.SpringFramework.petClinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/test/java/com/SpringFramework/petClinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.SpringFramework.petClinic.model;
+
+public class Vet extends Person {
+}
