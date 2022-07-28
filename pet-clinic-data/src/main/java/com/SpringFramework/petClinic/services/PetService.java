1dd748964d7a065a0580aac22d143d5b7e361563
Karthi678 <1801023c@cit.edu.in>
2022-07-28 11:24:51 +0530

    Interface is cleaned a bit and CrudService were introduced to reduce duplicate code

--- pet-clinic-data/src/main/java/com/SpringFramework/petClinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/SpringFramework/petClinic/services/PetService.java
@@ -2,11 +2,7 @@ package com.SpringFramework.petClinic.services;
 
 import com.SpringFramework.petClinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet,Long> {
 
-public interface PetService {
 
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/com/SpringFramework/petClinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/SpringFramework/petClinic/services/VetService.java
@@ -2,12 +2,7 @@ package com.SpringFramework.petClinic.services;
 
 import com.SpringFramework.petClinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
+public interface VetService extends CrudService<Vet,Long>{
 
 
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
